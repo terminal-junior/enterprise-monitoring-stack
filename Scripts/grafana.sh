a44a97d1532d47ae2f177ea5f5e446953d76115e
Junior <jj.josijunior@gmail.com>
2025-12-10 12:16:59 -0300

    Suppress output during Grafana installation steps
    Redirect output of commands to /dev/null to reduce console clutter during Grafana installation.

--- Scripts/grafana.sh
+++ Scripts/grafana.sh
@@ -4,44 +4,44 @@ echo "======================================="
 echo "  Instalação e Configuração do Grafana"
 echo "======================================="
 
-hostnamectl set-hostname Grafana
+hostnamectl set-hostname Grafana &>/dev/null
 
 # 1. Atualizando o sistema
 echo "[1/8] Atualizando o sistema..."
-dnf update -y
+dnf update -y &>/dev/null
 
 # 2. Instalando pacotes essenciais
 echo "[2/8] Instalando pacotes essenciais..."
-dnf install -y nano openssh-server firewalld
+dnf install -y nano openssh-server firewalld &>/dev/null
 
 # 3. Habilitando e iniciando serviços
 echo "[3/8] Habilitando serviços SSH e Firewalld..."
-systemctl enable sshd firewalld
-systemctl start sshd firewalld
+systemctl enable sshd firewalld &>/dev/null
+systemctl start sshd firewalld &>/dev/null
 
 # 4. Instalando Grafana Enterprise 12.3.0
 echo "[4/8] Instalando Grafana Enterprise..."
-yum install -y https://dl.grafana.com/grafana-enterprise/release/12.3.0/grafana-enterprise_12.3.0_19497075765_linux_amd64.rpm
+yum install -y https://dl.grafana.com/grafana-enterprise/release/12.3.0/grafana-enterprise_12.3.0_19497075765_linux_amd64.rpm &>/dev/null
 
 # 5. Recarregando serviços
 echo "[5/8] Recarregando daemon..."
-systemctl daemon-reload
+systemctl daemon-reload &>/dev/null
 
 # 6. Iniciando e habilitando o Grafana
 echo "[6/8] Iniciando e habilitando grafana-server..."
-systemctl start grafana-server
-systemctl enable grafana-server
+systemctl start grafana-server &>/dev/null
+systemctl enable grafana-server &>/dev/null
 
 # 7. Configurando firewall
 echo "[7/8] Configurando firewall..."
 
 # Libera Grafana para rede 192.168.1.0/24
-firewall-cmd --permanent --add-rich-rule='rule family="ipv4" source address="192.168.1.0/24" port port="3000" protocol="tcp" accept'
+firewall-cmd --permanent --add-rich-rule='rule family="ipv4" source address="192.168.1.0/24" port port="3000" protocol="tcp" accept' &>/dev/null
 
 # Libera SSH para rede 192.168.1.0/24
-firewall-cmd --permanent --add-rich-rule='rule family="ipv4" source address="192.168.1.0/24" service name="ssh" accept'
+firewall-cmd --permanent --add-rich-rule='rule family="ipv4" source address="192.168.1.0/24" service name="ssh" accept' &>/dev/null
 
-firewall-cmd --reload
+firewall-cmd --reload &>/dev/null
 
 echo "[8/8] Instalação finalizada!"
 
